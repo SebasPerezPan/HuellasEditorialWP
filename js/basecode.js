7c97c99302842635bf0b8e9fb7372ec99602a513
Sebastian Perez <sebasperezpan@gmail.com>
2023-04-07 01:03:44 -0500

    First adding

---
+++ js/basecode.js
@@ -0,0 +1,12 @@
+function inicio() {
+
+    let bt_in = document.getElementById("btn_i")
+    bt_in.addEventListener("click", mensages)
+
+}
+
+function bu_in () {
+    
+}
+
+window.addEventListener("load", inicio)
