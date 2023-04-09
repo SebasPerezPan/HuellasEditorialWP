c13f6222a27696e2eca09fa3e0dd8a2ed9276b18
Sebastian Perez <sebasperezpan@gmail.com>
2023-04-09 13:39:09 -0500

    Adding some tools and stuff that i learn this wknd

--- js/basecode.js
+++ js/basecode.js
@@ -1,12 +1,9 @@
 function inicio() {
 
-    let bt_in = document.getElementById("btn_i")
-    bt_in.addEventListener("click", mensages)
 
 }
 
-function bu_in () {
     
-}
+
 
 window.addEventListener("load", inicio)
